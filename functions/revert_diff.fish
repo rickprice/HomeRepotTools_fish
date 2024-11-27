8974e0a7799c231edad4870e16a058e3866b374b
Frederick Price <rprice@pricemail.ca>
2024-11-26 19:58:40 -0500

    Get rid of warning on egrep

--- functions/revert_diff.fish
+++ functions/revert_diff.fish
@@ -2,5 +2,5 @@ function revert_diff
     set NAMESPACE $argv[1]
     set INGREDIENT $argv[2]
     set VERSION $argv[3]
-   diff (ingredient search -es $NAMESPACE -n $INGREDIENT -v $VERSION -d --json -k1 | psub)  (ingredient search -es $NAMESPACE -n $INGREDIENT -v $VERSION -d --json | psub) | egrep 'feature|namespace|patch|option'
+   diff (ingredient search -es $NAMESPACE -n $INGREDIENT -v $VERSION -d --json -k1 | psub)  (ingredient search -es $NAMESPACE -n $INGREDIENT -v $VERSION -d --json | psub) | grep -E 'feature|namespace|patch|option'
 end
