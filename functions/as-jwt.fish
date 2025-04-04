a192b67a3d4d9a5e2d70abfeda6ce41062ccb25f
Frederick Price <rprice@pricemail.ca>
2025-04-04 18:00:36 -0400

    More improvements

--- functions/as-jwt.fish
+++ functions/as-jwt.fish
@@ -1,9 +1,9 @@
 function as-jwt
-    state-svc stop
-    state auth --prompt
-    echo "Putting AS JWT on clipboard"
+    state-svc stop 2>&1 >/dev/null
+    state auth --prompt 2>&1 >/dev/null
+
+    echo "Putting AS JWT on clipboard" and into '$AS_JWT'
 
     set AS_JWT (state export jwt 2>&1 | awk '/Exporting Credentials/ {found=1; next} found==1 {found++; next} found==2 {print "{\"Authorization\": \"Bearer "$0"\"}"; exit}')
-
     echo $AS_JWT | xclip -in -selection clipboard
 end
