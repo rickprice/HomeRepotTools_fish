e8442e14e60d4b9c8e308000a7835fb7fafe403c
Frederick Price <rprice@pricemail.ca>
2025-04-04 17:51:11 -0400

    Fix another type

--- functions/as-jwt.fish
+++ functions/as-jwt.fish
@@ -5,5 +5,5 @@ function as-jwt
 
     set AS_JWT (state export jwt 2>&1 | awk '/Exporting Credentials/ {found=1; next} found==1 {found++; next} found==2 {print "{\"Authorization\": \"Bearer "$0"\"}"; exit}')
 
-    echo $AS_JTW | xclip -in -selection clipboard
+    echo $AS_JWT | xclip -in -selection clipboard
 end
