14d1eb12cf63fc559047e8b5b80812acc89e9b02
Frederick Price <rprice@pricemail.ca>
2025-04-04 17:41:17 -0400

    Trying another fix

--- functions/as-jwt.fish
+++ functions/as-jwt.fish
@@ -1,7 +1,5 @@
 function as-jwt
     state-svc stop
     state auth --prompt
-    echo "Putting AS JWT on clipboard"
-
     state export jwt 2>&1 | awk '/Exporting Credentials/ {found=1; next} found==1 {found++; next} found==2 {print "{\"Authorization\": \"Bearer "$0"\"}"; exit}' | xclip
 end
